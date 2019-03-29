05514498a2aa58d6132b49b95b4f5e79323cd38d
Krijn van der Steen <krijnvdsteen@live.nl>
2019-03-29 15:32:43 +0100

    Add Team and Player class
    Added Team and Player class, where only Team contains usefull methods. Also switched ClubFiller.cs position.

---
+++ Assets/ClubFiller.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        string[] lastNames;
+        string[] frontNames;
+        string[] playerNames;
+
+        string[] teams;
+        
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            //program.AssignTeamQuality();
+        }
+
+        void FillPlayerFile()
+        {
+            lastNames = File.ReadAllLines(@"C:\Users\Krijn\Documents\Football\Football App\last name.txt", Encoding.UTF8);
+            frontNames = File.ReadAllLines(@"C:\Users\Krijn\Documents\Football\Football App\front name.txt", Encoding.UTF8);
+            playerNames = new string[1500];
+
+            int lastNamesCount = lastNames.Length;
+            int frontNamesCount = frontNames.Length;
+
+            Random random = new Random();
+
+            for(int i = 0; i < 1500; i++)
+            {
+                int frontNameInt = random.Next(frontNamesCount);
+                int lastNameInt = random.Next(lastNamesCount);
+                string name = frontNames[frontNameInt] + " " + lastNames[lastNameInt];
+                playerNames[i] = name;
+                Console.WriteLine(name);
+            }
+
+            File.WriteAllLines(@"C:\Users\Krijn\Documents\Football\Football App\player names.txt", playerNames);
+        }
+
+        void MakeCitiesClubs()
+        {
+            teams = File.ReadAllLines(@"C:\Users\Krijn\Documents\Football\teams.txt", Encoding.UTF8);
+
+            int teamCount = teams.Length;
+
+            Random random = new Random();
+
+            string[] additions = { " United", " FC", " City", " SC", " Club"};
+
+            int additionsCount = additions.Length;
+            
+            for(int i = 0; i < teamCount; i++)
+            {
+                int addition = random.Next(additionsCount);
+                teams[i] += additions[addition];
+            }
+
+            File.WriteAllLines(@"C:\Users\Krijn\Documents\Football\teams.txt", teams);
+        }
+
+        void AssignTeamColours()
+        {
+            teams = File.ReadAllLines(@"C:\Users\Krijn\Documents\Football\teams.txt", Encoding.UTF8);
+
+            int teamCount = teams.Length;
+
+            Random random = new Random();
+            
+            string[] additions = {"Black", "Blue", "Cyan", "DarkBlue", "DarkRed", "Gray", "Green", "Magenta", "Red", "White", "Yellow"};
+
+            int additionsCount = additions.Length;
+
+            for (int i = 0; i < teamCount; i++)
+            {
+                int homeColor = random.Next(additionsCount);
+                int awayColor = random.Next(additionsCount);
+                while(homeColor == awayColor)
+                {
+                    awayColor = random.Next(additionsCount);
+                }
+
+                teams[i] += ",ConsoleColor." + additions[homeColor] + ",ConsoleColor." + additions[awayColor];
+            }
+
+            File.WriteAllLines(@"C:\Users\Krijn\Documents\Football\teams.txt", teams);
+        }
+
+        void AssignPlayersToTeams()
+        {
+            playerNames = File.ReadAllLines(@"C:\Users\Krijn\Documents\Football\Football App\player names.txt", Encoding.UTF8);    
+
+            Random random = new Random();
+
+            teams = File.ReadAllLines(@"C:\Users\Krijn\Documents\Football\teams.txt", Encoding.UTF8);
+
+            ArrayList tempTeams = new ArrayList();
+
+            int teamCount = teams.Length;
+
+            for(int i = 0; i < teamCount; i++)
+            {
+                tempTeams.Add(teams[i]);
+                for(int j = 0; j < 22; j++)
+                {
+                    int playerCount = playerNames.Length;
+
+                    int randomPlayer = random.Next(playerCount);
+
+                    tempTeams.Add(playerNames[randomPlayer]);
+
+                    playerNames = playerNames.Where(val => val != playerNames[randomPlayer]).ToArray();
+                }
+            }
+
+            File.WriteAllLines(@"C:\Users\Krijn\Documents\Football\teams.txt", tempTeams.ToArray(typeof(string)) as string[]);
+        }
+
+        void AssignPlayerPositions()
+        {
+            Random random = new Random();
+
+            teams = File.ReadAllLines(@"C:\Users\Krijn\Documents\Football\teams.txt", Encoding.UTF8);
+
+            int teamCount = teams.Length;
+
+            int counter = 0;
+
+            for (int i = 0; i < teamCount / 23; i++)
+            {
+                counter++;
+                for (int j = 0; j < 22; j++)
+                {
+                    if (j < 2)
+                    {
+                        teams[counter] += ",GK";
+                    }
+                    else if (j < 4)
+                    {
+                        teams[counter] += ",RB";
+                        if (random.Next(0, 2) == 1)
+                        {
+                            teams[counter] += "/RWB";
+                        }
+                    }
+                    else if (j < 8)
+                    {
+                        teams[counter] += ",CB";
+                        if (random.Next(0, 2) == 1)
+                        {
+                            teams[counter] += "/CDM";
+                        }
+                    }
+                    else if (j < 10)
+                    {
+                        teams[counter] += ",LB";
+                        if (random.Next(0, 2) == 1)
+                        {
+                            teams[counter] += "/LWB";
+                        }
+                    }
+                    else if (j < 16)
+                    {
+                        teams[counter] += ",CM";
+                        if (random.Next(0, 2) == 1)
+                        {
+                            teams[counter] += "/CAM";
+                        }
+                    }
+                    else if (j < 18)
+                    {
+                        teams[counter] += ",RW";
+                        if (random.Next(0, 2) == 1)
+                        {
+                            teams[counter] += "/RM";
+                        }
+                    }
+                    else if (j < 20)
+                    {
+                        teams[counter] += ",ST";
+                        if (random.Next(0, 2) == 1)
+                        {
+                            teams[counter] += "/CF";
+                        }
+                    }
+                    else if (j < 22)
+                    {
+                        teams[counter] += ",LW";
+                        if (random.Next(0, 2) == 1)
+                        {
+                            teams[counter] += "/LM";
+                        }
+                    }
+                    counter++;
+                }
+            }
+
+            File.WriteAllLines(@"C:\Users\Krijn\Documents\Football\teams.txt", teams);
+        }
+
+        void AssignAge()
+        {
+            Random random = new Random();
+
+            teams = File.ReadAllLines(@"C:\Users\Krijn\Documents\Football\teams.txt", Encoding.UTF8);
+            
+            int teamCount = teams.Length;
+
+            int counter = 0;
+
+            for (int i = 0; i < teamCount/23; i++)
+            {
+                counter++;
+                for (int j = 0; j < 22; j++)
+                {
+                    int firstAge = random.Next(16, 36);
+                    int secondAge = random.Next(16, 36);
+
+                    int averageAge = (firstAge + secondAge) / 2;
+
+                    teams[counter] += "," + averageAge;
+
+                    counter++;
+
+                }
+            }
+
+            File.WriteAllLines(@"C:\Users\Krijn\Documents\Football\teams.txt", teams);
+        }
+
+        void AssignTeamQuality()
+        {
+            Random random = new Random();
+
+            teams = File.ReadAllLines(@"C:\Users\Krijn\Documents\Football\teams.txt", Encoding.UTF8);
+
+            int teamCount = teams.Length;
+
+            for (int i = 0; i < teamCount; i += 23)
+            {
+                if(i < 18 * 23)
+                {
+                    teams[i] += "," + random.Next(16, 21);
+                }
+                else if(i < 18 * 46)
+                {
+                    teams[i] += "," + random.Next(13, 17);
+                }
+                else
+                {
+                    teams[i] += "," + random.Next(9, 15);
+                }
+            }
+
+            File.WriteAllLines(@"C:\Users\Krijn\Documents\Football\teams.txt", teams);
+        }
+    }
+}
---
+++ Assets/Team.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class Team : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    string[] teamsFile;
+
+    Team[] teams;
+
+    int teamCount;
+
+    System.Random random;
+
+    private string teamName { get; set; }
+    private ConsoleColor homeColor { get; set; }
+    private ConsoleColor awayColor { get; set; }
+    private int reputation { get; set; }
+    private Player[] players { get; set; }
+
+
+
+    public Team(string teamName, ConsoleColor homeColor, ConsoleColor awayColor, int reputation, Player[] players)
+    {
+        this.teamName = teamName;
+        this.homeColor = homeColor;
+        this.awayColor = awayColor;
+        this.reputation = reputation;
+        this.players = players;
+    }
+
+    void Start()
+    {
+        teamsFile = File.ReadAllLines(@"C:\Users\Krijn\Documents\Football\teams.txt", Encoding.UTF8);
+
+        teamCount = teamsFile.Length;
+
+        random = new System.Random();
+
+        teams = InitTeams();
+
+        for(int i = 0; i < teams.Length; i++)
+        {
+            Debug.Log(teams[i].teamName);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    Team[] InitTeams()
+    {
+        Team newTeam = null;
+
+        Team[] tempTeams = new Team[teamCount/23];
+
+        for(int i = 0; i < teamCount; i += 23)
+        {
+            string[] components = teamsFile[i].Split(',');
+            newTeam.teamName = components[0];
+            newTeam.homeColor = (ConsoleColor) Enum.Parse(typeof(ConsoleColor), components[1]);
+            newTeam.awayColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), components[2]);
+            newTeam.reputation = Convert.ToInt32(components[3]);
+
+            tempTeams[i / 23] = newTeam;
+
+            //for(int j = i + 1; i < j + 23; j++)
+            //{
+
+            //}
+        }
+
+        return tempTeams;
+    }
+}
